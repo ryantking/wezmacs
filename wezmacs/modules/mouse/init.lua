bfed29bcae786fafb4ad645852ffa191697abc4c
Ryan King <ryan.king@flipsidecrypto.com>
2025-11-30 15:36:36 -0500

    Update init.lua: moderate changes (41 lines)

--- wezmacs/modules/mouse/init.lua
+++ wezmacs/modules/mouse/init.lua
@@ -18,25 +18,19 @@ local M = {}
 
 M._NAME = "mouse"
 M._CATEGORY = "behavior"
-M._VERSION = "0.1.0"
 M._DESCRIPTION = "Mouse bindings and behavior"
 M._EXTERNAL_DEPS = {}
-M._FEATURE_FLAGS = {}
+M._FEATURES = {}
 M._CONFIG_SCHEMA = {
   leader_mod = "CMD",
 }
 
-function M.init(enabled_flags, user_config, log)
-  local config = {}
-  for k, v in pairs(M._CONFIG_SCHEMA) do
-    config[k] = user_config[k] or v
-  end
-  return { config = config, flags = enabled_flags or {} }
-end
+function M.apply_to_config(config)
+  -- Get configuration
+  local mod_config = wezmacs.get_config(M._NAME)
 
-function M.apply_to_config(config, state)
   config.alternate_buffer_wheel_scroll_speed = 1
-  config.bypass_mouse_reporting_modifiers = state.config.leader_mod
+  config.bypass_mouse_reporting_modifiers = mod_config.leader_mod
   config.hide_mouse_cursor_when_typing = false
 
   config.mouse_bindings = {
@@ -49,7 +43,7 @@ function M.apply_to_config(config, state)
     -- Leader+left-click: Open link or extend selection
     {
       event = { Up = { streak = 1, button = "Left" } },
-      mods = state.config.leader_mod,
+      mods = mod_config.leader_mod,
       action = wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor("ClipboardAndPrimarySelection"),
     },
 
