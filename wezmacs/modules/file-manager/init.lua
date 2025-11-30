525baa2b79a6c8f2600d59a9f3ae72cd4e3a9ce4
Ryan King <ryan.king@flipsidecrypto.com>
2025-11-30 15:39:06 -0500

    Update init.lua: major changes (55 lines)

--- wezmacs/modules/file-manager/init.lua
+++ wezmacs/modules/file-manager/init.lua
@@ -20,23 +20,14 @@ local M = {}
 
 M._NAME = "file-manager"
 M._CATEGORY = "tools"
-M._VERSION = "0.1.0"
 M._DESCRIPTION = "File management with yazi terminal file manager"
 M._EXTERNAL_DEPS = { "yazi" }
-M._FEATURE_FLAGS = {}
+M._FEATURES = {}
 M._CONFIG_SCHEMA = {
   file_manager = "yazi",
   leader_key = "f",
   leader_mod = "LEADER",
 }
-
-function M.init(enabled_flags, user_config, log)
-  local config = {}
-  for k, v in pairs(M._CONFIG_SCHEMA) do
-    config[k] = user_config[k] or v
-  end
-  return { config = config, flags = enabled_flags or {} }
-end
 
 function M.apply_to_config(wezterm_config, state)
   local split = require("wezmacs.utils.split")
