55010a52d024ddf08626a22b6e7b9abac11f91e4
Ryan King <ryan.king@flipsidecrypto.com>
2025-12-01 18:09:38 -0500

    Update init.lua: minor changes (6 lines)

--- wezmacs/modules/editors/init.lua
+++ wezmacs/modules/editors/init.lua
@@ -33,26 +33,26 @@ M._CONFIG = {
 }
 
 function M.apply_to_config(wezterm_config)
-  local mod_config = wezmacs.get_config(M._NAME)
+  local mod = wezmacs.get_module(M._NAME)
   local split = require("wezmacs.utils.split")
 
   -- Terminal editor in smart split
   local function terminal_editor_split(window, pane)
-    split.smart_split(pane, { mod_config.terminal_editor, "." })
+    split.smart_split(pane, { mod.terminal_editor, "." })
   end
 
   -- IDE launcher
   local function launch_ide(window, pane)
     local cwd_uri = pane:get_current_working_dir()
     local cwd = cwd_uri and cwd_uri.file_path or wezterm.home_dir
-    wezterm.background_child_process({ mod_config.ide, cwd })
+    wezterm.background_child_process({ mod.ide, cwd })
   end
 
   -- Create editors key table
   wezterm_config.key_tables = wezterm_config.key_tables or {}
   wezterm_config.key_tables.editors = {
     { key = "t", action = wezterm.action_callback(terminal_editor_split) },
-    { key = "T", action = act.SpawnCommandInNewTab({ args = { mod_config.terminal_editor, "." } }) },
+    { key = "T", action = act.SpawnCommandInNewTab({ args = { mod.terminal_editor, "." } }) },
     { key = "i", action = wezterm.action_callback(launch_ide) },
     { key = "Escape", action = "PopKeyTable" },
   }
@@ -60,8 +60,8 @@ function M.apply_to_config(wezterm_config)
   -- Add keybinding to activate editors menu
   wezterm_config.keys = wezterm_config.keys or {}
   table.insert(wezterm_config.keys, {
-    key = mod_config.leader_key,
-    mods = mod_config.leader_mod,
+    key = mod.leader_key,
+    mods = mod.leader_mod,
     action = act.ActivateKeyTable({
       name = "editors",
       one_shot = false,
