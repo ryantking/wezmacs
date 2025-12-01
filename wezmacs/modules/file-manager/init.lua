ebd3d7fd68bba97cf3893a63bca1fd182214b192
Ryan King <ryan.king@flipsidecrypto.com>
2025-12-01 18:11:24 -0500

    Update init.lua: moderate changes (45 lines)

--- wezmacs/modules/file-manager/init.lua
+++ wezmacs/modules/file-manager/init.lua
@@ -25,42 +25,41 @@ M._NAME = "file-manager"
 M._CATEGORY = "tools"
 M._DESCRIPTION = "File management with yazi terminal file manager"
 M._EXTERNAL_DEPS = { "yazi" }
-M._FEATURES = {}
-M._CONFIG_SCHEMA = {
+M._CONFIG = {
   file_manager = "yazi",
   leader_key = "f",
   leader_mod = "LEADER",
 }
 
 function M.apply_to_config(wezterm_config)
-  local mod_config = wezmacs.get_config(M._NAME)
+  local mod = wezmacs.get_module(M._NAME)
   local split = require("wezmacs.utils.split")
 
   -- File manager in smart split
   local function file_manager_split(window, pane)
-    split.smart_split(pane, { mod_config.file_manager })
+    split.smart_split(pane, { mod.file_manager })
   end
 
   -- File manager with sudo in smart split
   local function file_manager_sudo_split(window, pane)
-    split.smart_split(pane, { "sudo", mod_config.file_manager, "/" })
+    split.smart_split(pane, { "sudo", mod.file_manager, "/" })
   end
 
   -- Create file-manager key table
   wezterm_config.key_tables = wezterm_config.key_tables or {}
   wezterm_config.key_tables["file-manager"] = {
     { key = "f", action = wezterm.action_callback(file_manager_split) },
-    { key = "F", action = act.SpawnCommandInNewTab({ args = { mod_config.file_manager } }) },
+    { key = "F", action = act.SpawnCommandInNewTab({ args = { mod.file_manager } }) },
     { key = "s", action = wezterm.action_callback(file_manager_sudo_split) },
-    { key = "S", action = act.SpawnCommandInNewTab({ args = { "sudo", mod_config.file_manager, "/" } }) },
+    { key = "S", action = act.SpawnCommandInNewTab({ args = { "sudo", mod.file_manager, "/" } }) },
     { key = "Escape", action = "PopKeyTable" },
   }
 
   -- Add keybinding to activate file-manager menu
   wezterm_config.keys = wezterm_config.keys or {}
   table.insert(wezterm_config.keys, {
-    key = mod_config.leader_key,
-    mods = mod_config.leader_mod,
+    key = mod.leader_key,
+    mods = mod.leader_mod,
     action = act.ActivateKeyTable({
       name = "file-manager",
       one_shot = false,
